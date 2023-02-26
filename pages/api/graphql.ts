6e0445d3bda13d9051417b0b043cc22f6f943470
Putra Nugraha <asiapatta10@gmail.com>
2023-02-26 09:25:01 +0700

    create authentication with okta and permission

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -2,12 +2,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { createYoga } from "graphql-yoga";
 
 import { schema } from "../../graphql/schema";
+import { createContext } from "../../graphql/context";
 
 export default createYoga<{
   req: NextApiRequest;
   res: NextApiResponse;
 }>({
   schema,
+  context: createContext,
   graphqlEndpoint: "/api/graphql",
 });
 
